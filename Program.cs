cdc03a4cdf75a65cf3b020dc6c922ef60cd6e54d
webdfabinho <webdfabinho@gmail.com>
2021-07-05 17:59:23 -0300

    Exemplos de C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DigitalInnovationOne.Exemplos;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main()
+        {
+           var s = new Pilha();
+           s.Empilha(1);
+           s.Empilha(10);
+           s.Empilha(100);
+           Console.WriteLine(s.Desempilha());
+           Console.WriteLine(s.Desempilha());
+           Console.WriteLine(s.Desempilha());
+        }
+    }
+}
